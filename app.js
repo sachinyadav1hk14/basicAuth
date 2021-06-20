aeafa77230baee0dd8acd8d8348fd099827e06dd
Sachin Yadav <sachinyadav1hk14@gmail.com>
2021-06-21 04:26:46 +0530

    updated to level 5 Secruity with Hashing salting and passport.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //jshint esversion:6
+
+///////////////////////////////////////////modules////////////////////////////////////////////////
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -12,67 +17,100 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/////////////////mongodb//////////
+////////////////////////////////////////passport setup///////////////////////////////////////
+app.use(session({
+ secret: "this is a long string",
+ resave: false,
+ saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+////////////////////////////////////////////mongodb///////////////////////////////////////////
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
 
+// collection format
 const userSchema = new mongoose.Schema({
  email: String,
  password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
-
+//collection items/users
 const User = new mongoose.model('User', userSchema);
 
-////////////////routes////////////
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//////////////////////////////////////////////routes//////////////////////////////////////////
 
 
 app.get('/', function (req, res) {
  res.render('home');
 });
-app.get('/logout', function (req, res) {
- res.render('home');
-});
-app.get('/login', function (req, res) {
- res.render('login');
-});
+
 app.get('/register', function (req, res) {
  res.render('register');
 });
-app.post('/register', function (req, res) {
- const newUser = new User({
-  email: req.body.username,
-  password: md5(req.body.password)
- });
- newUser.save(function (err) {
-  if (err) {
-   console.log(err);
-  } else {
-   res.render('secrets');
-  }
- });
-});
-app.post('/login', function (req, res) {
- const username = req.body.username;
- const password = md5(req.body.password);
- 
- User.findOne({ email: username }, function (err, foundUser) {
-  if (err) {
-   console.log(err);
-  } else {
-   if (foundUser) {
-    if (foundUser.password === password) {
-     res.render('secrets');
-    }
-   }
-  }
- });
+
+app.get('/secrets', function (req, res) {
+ if (req.isAuthenticated()) {
+  res.render('secrets');
+ } else {
+  res.redirect('/login');
+ }
 });
 
-////////////////port setup/////////////
+app.post('/register', function (req, res) {
+ User.register({ username: req.body.username }, req.body.password, function (err, user) {
+  if (err) {
+   console.log(err);
+   res.redirect('/register');
+  } else {
+   passport.authenticate('local')(req, res, function () {
+    res.redirect('/secrets')
+   })
+  }
+ })
+});
+
+app.get('/logout', function (req, res) {
+ req.logOut();
+ res.redirect('/');
+});
+
+app.get('/login', function (req, res) {
+ res.render('login');
+});
+
+app.post('/login', function (req, res) {
+ const user = new User({
+  username: req.body.username,
+  password: req.body.password
+ });
+
+ req.login(user, function (err) {
+  if (err) {
+   console.log(err);
+  } else {
+   passport.authenticate('local')(req, res, function () {
+    res.redirect("/secrets");
+   })
+  }
+ })
+
+});
+
+///////////////////////////////////////////////port setup////////////////////////////////////////////
 
 const port = process.env.port || 3000;
 app.listen(port, function () {
  console.log(`Server is running on ${port}`)
-})
+});
